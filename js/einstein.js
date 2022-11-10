cb6cc7d835775dc848b867efb951cce9a471ba22
Veronika Ulybina <veronica.ulybina@yandex.ru>
2022-11-10 07:39:59 +0500

    init

---
+++ js/einstein.js
@@ -0,0 +1,6 @@
+
+const myWeight = 55;
+const speedLight= 3e8;
+
+const bodyEnergy = myWeight * speedLight ** 2;
+console.log(bodyEnergy);
--- js/script.js
+++ js/script.js
@@ -8,7 +8,24 @@
 
   const productCount = 22;
   const productPrice = 150;
-  
+
   const productSum = productCount * productPrice;
   console.log(`Общая сумма товара: ${productSum}`);
+}
+
+{
+  const getProductName = prompt('Наименование товара? (Яблоки/груши/морковь/свекла)');
+  console.log(typeof getProductName);
+
+  const getProductCategory = prompt('Категория товара? (Фрукты/овощи)');
+  console.log(typeof getProductCategory);
+
+  const getProductCount = prompt('Количество товара?');
+  console.log(typeof +getProductCount);
+
+  const getProductPrice = prompt('Цена товара? ($)');
+  console.log(typeof +getProductPrice);
+
+  const getProductSum = getProductCount * getProductPrice;
+  console.log(`На складе ${getProductCount} единицы товара "${getProductName}" на сумму ${getProductSum} долларов`)
 }
